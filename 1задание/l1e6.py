5e480e054751a7cefef05c4728e429d0a286e182
Elzeko <andreyelzeko@mail.ru>
2021-09-29 22:17:28 +0300

    1 задание

---
+++ 1задание/l1e6.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+x = 2
+y = 3
+a = y
+y = x
+x = a
+print(x,y)
---
+++ 1задание/l1e7.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+number = 16
+if number % 2 == 0:
+  print('чётное')
+elif number % 2 != 0: 
+    print('не чётное')
