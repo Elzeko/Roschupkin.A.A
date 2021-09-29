9f816c4b3a883cb96bb71b064bb3a4a8055d4097
Elzeko <61206725+Elzeko@users.noreply.github.com>
2021-09-29 21:15:22 +0300

    l1e4 Внесены изменения
    Неправильно понял условие

--- l1e4.py
+++ l1e4.py
@@ -1,6 +1,7 @@
 # -- coding: utf-8 --
-seconds = 86400
-days = seconds / 86400
-hours = seconds / 3600
-minutes = seconds / 60
+s = 10000
+days = s // 86400 
+hours = (s % 86400) // 3600
+minutes = ((s % 86400) % 3600) // 60
+seconds = (((s % 86400) % 3600) % 60) % 60 
 print (days ,'дней:', hours ,'часов:', minutes ,'минут:', seconds ,'секунд')
